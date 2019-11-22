c06f9a1c6922f876d7bf90cf068368760d0d0605
jcharbo <jcharbo@users.noreply.github.com>
2019-11-22 11:41:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,19 @@
 import { NativeModules, NativeEventEmitter } from 'react-native';
 
-const { ZiggeoPlayer } = NativeModules;
-const { ZiggeoRecorder } = NativeModules;
+const { RCTZiggeoPlayer } = NativeModules;
 
 export default {
 	// ZiggeoRecorder
 	setAppToken: function (appToken: string) {
-		ZiggeoPlayer.setAppToken(appToken);
-		ZiggeoRecorder.setAppToken(appToken);
+    global.warn('aaa',NativeModules)
+    RCTZiggeoPlayer.setAppToken(appToken);
   	},
-	setAutostartRecordingAfter: function (seconds) {
-		ZiggeoRecorder.setAutostartRecordingAfter(seconds);
-  	},
-  	/**
- 	  * @deprecated Use `setExtraArgsForRecorder` instead.
- 	  */
-	setExtraArgsForCreateVideo: function (map) {
-		ZiggeoRecorder.setExtraArgsForCreateVideo(map);
-  	},
-	setExtraArgsForRecorder: function (map) {
-		ZiggeoRecorder.setExtraArgsForRecorder(map);
-  	},
-	setCoverSelectorEnabled: function (enabled) {
-		ZiggeoRecorder.setCoverSelectorEnabled(enabled);
-  	},
-	setMaxRecordingDuration: function (seconds) {
-		ZiggeoRecorder.setMaxRecordingDuration(seconds);
-  	},
-	setCameraSwitchEnabled: function (enabled) {
-		ZiggeoRecorder.setCameraSwitchEnabled(enabled);
-  	},
-	setSendImmediately: function (sendImmediately) {
-		ZiggeoRecorder.setSendImmediately(sendImmediately);
-  	},
-	setQuality: function (quality) {
-		ZiggeoRecorder.setQuality(quality);
-  	},
-	setCamera: function (camera) {
-		ZiggeoRecorder.setCamera(camera);
-  	},
-	record: async function () {
-		return ZiggeoRecorder.record();
-  	},
-	uploadFromFileSelector: async function () {
-		return ZiggeoRecorder.uploadFromFileSelector(0, false);
-	},
-	uploadFromPath: async function (fileName) {
-		return ZiggeoRecorder.uploadFromPath(fileName);
-	},
-	uploadFromFileSelectorWithDurationLimit: async function (maxAllowedDurationInSeconds, enforceDuration) {
-		if(!enforceDuration){
-			enforceDuration = false;
-		}
-		return ZiggeoRecorder.uploadFromFileSelector(maxAllowedDurationInSeconds, enforceDuration);
-	},
-	cancelRequest: function () {
-		ZiggeoRecorder.cancelRequest();
-  	},
-	recorderEmitter: function() {
-		return recorderEmitter = new NativeEventEmitter(ZiggeoRecorder);
-	},
-
-  	// ZiggeoPlayer
   	play: function (videoId: string) {
-		ZiggeoPlayer.play(videoId);
+      RCTZiggeoPlayer.play(videoId);
   	},
 	setExtraArgsForPlayer: function (map) {
-		ZiggeoPlayer.setExtraArgsForPlayer(map);
+    RCTZiggeoPlayer.setExtraArgsForPlayer(map);
   	},
   	
-  	// Constants
-  	REAR_CAMERA: ZiggeoRecorder.rearCamera,
-  	FRONT_CAMERA: ZiggeoRecorder.frontCamera,
-  	HIGH_QUALITY: ZiggeoRecorder.highQuality,
-  	MEDIUM_QUALITY: ZiggeoRecorder.mediumQuality,
-  	LOW_QUALITY: ZiggeoRecorder.lowQuality
+
  };
